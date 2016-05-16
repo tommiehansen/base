145006596e2f0a0b8a5f66fbf70a996a2900cca5
Tommie Hansen <tommiehansen@gmail.com>
2016-05-16 04:35:48 +0200

    Added Stagger animations
    A small javascript func to stagger css animations and transitions.

---
+++ js/animation.stagger/animation.stagger.js
@@ -0,0 +1,29 @@
+/*
+
+    Stagger animations
+    Simply add delays to any animation or transition
+
+    ! Stagger animation (C) 2016 Tommie Hansen. MIT license --> en.wikipedia.org/wiki/MIT_License
+
+*/
+
+var stagger = {
+
+    init: function(el, stag, stop, type){
+
+        stag = stag || 60;
+        stop = stop || null;
+        type = type || 'animation-delay';
+
+        var el = document.querySelectorAll(el),
+            len = el.length, stagger;
+
+        for(var i=0;i<len;i++){
+            if(stop && stop < i) { stagger = stag * stop; }
+            else { stagger = stag * i; }
+            el[i].style = type + ':' + stagger + 'ms';
+        }
+
+    }
+
+};
